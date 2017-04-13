b82d17d74d4dda16df81f4d3d32707c832b2b263
RAN-PC\Ran Kuang <rkuang@id.iit.edu>
2017-04-12 22:29:48 -0500

    fixed the LOGICAL OPERATOR but doesn't work???

--- script.js
+++ script.js
@@ -8,25 +8,25 @@ console.log(n);
 function boxClicked() {
     console.log("box clicked")
     switch(n) {
-        case 201<= n <=300:
+        case n>=201 && n<=300:
            boxes.style.backgroundColor = "yellow";
            break;
-        case 301<= n <=400:
+        case n>=301 && n <=400:
            boxes.style.backgroundColor = "green";
            break;
-        case 401<= n <=500:
+        case n>=401 && n <=500:
            boxes.style.backgroundColor = "purple";
            break;
-        case 501<= n <=600:
+        case n>=501 && n <=600:
            boxes.style.backgroundColor = "orange";
            break;
-        case 601<= n <=700:
+        case n>=601 && n <=700:
            boxes.style.backgroundColor = "pink";
            break;
-        case 701<= n <=800:
+        case n>=701 && n <=800:
            boxes.style.backgroundColor = "black";
            break;
-        case 801<= n <=900:
+        case n>=801 && n <=900:
            boxes.style.backgroundColor = "brown";
            break;
         default:

3629f71eeb47165e65d91c6a50fc986c1a034b95
RAN-PC\Ran Kuang <rkuang@id.iit.edu>
2017-04-12 22:21:48 -0500

    don't know how to select index and how to set the n>=201 and n<=300

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
 // Do your work in this file.
+
+var boxes = document.querySelectorAll(".box");
+var n = boxes.length;
+
+console.log(n);
+
+function boxClicked() {
+    console.log("box clicked")
+    switch(n) {
+        case 201<= n <=300:
+           boxes.style.backgroundColor = "yellow";
+           break;
+        case 301<= n <=400:
+           boxes.style.backgroundColor = "green";
+           break;
+        case 401<= n <=500:
+           boxes.style.backgroundColor = "purple";
+           break;
+        case 501<= n <=600:
+           boxes.style.backgroundColor = "orange";
+           break;
+        case 601<= n <=700:
+           boxes.style.backgroundColor = "pink";
+           break;
+        case 701<= n <=800:
+           boxes.style.backgroundColor = "black";
+           break;
+        case 801<= n <=900:
+           boxes.style.backgroundColor = "brown";
+           break;
+        default:
+           boxes.style.backgroundColor = "blue";
+           break;
+    }
+}
+
+
+
+for (var i = 0; i < n; i++) {
+    boxes[i].addEventListener("click", boxClicked);
+}
